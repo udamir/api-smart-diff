4e1ba2fbc6e522a7dd2cf237d02cff7f0d59e7da
Damir <damir@yusipov.ru>
2022-04-06 01:27:59 +0300

    test added

--- test/servicenode-merge.test.ts
+++ test/servicenode-merge.test.ts
@@ -6,12 +6,20 @@ const exampleResource = new ExampleResource("serviceNode.yaml", serviceNodeRules
 
 describe("Test service-node merge", () => {
   it("remove operation should be breaking change in meta", () => {
-    const path = ["children", 2]
-
-    const after = exampleResource.clone([removePatch(path)])
+    const after = exampleResource.clone([removePatch(["children", 2])])
     const merged = exampleResource.merge(after, { metaKey, arrayMeta: true })
 
     const meta = merged.children[metaKey]
     expect(meta).toMatchObject({ 2: { action: DiffAction.remove, type: breaking } })
+  })
+})
+
+describe("Test service-node merge", () => {
+  it("remove operation should be breaking change in meta", () => {
+    const after = exampleResource.clone([removePatch(["children", 2])])
+    const merged = exampleResource.merge(after, { metaKey, arrayMeta: false })
+
+    const meta = merged[metaKey]
+    expect(meta.children.array).toMatchObject({ 2: { action: DiffAction.remove, type: breaking } })
   })
 })
