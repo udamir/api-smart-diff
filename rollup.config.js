0d1698479c4d4754bea7225f3bf563fa17930709
Damir <damir@yusipov.ru>
2024-07-06 01:09:57 +0300

    fix formating

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
-import babel from '@rollup/plugin-babel';
-import filesize from 'rollup-plugin-filesize';
-import progress from 'rollup-plugin-progress';
+import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
-import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
+import filesize from 'rollup-plugin-filesize'
+import progress from 'rollup-plugin-progress'
+import terser from '@rollup/plugin-terser'
+import babel from '@rollup/plugin-babel'
+import json from '@rollup/plugin-json'
+
+import pkg from './package.json'
 
 const packageName = 'ApiSmartDiff'
 const inputPath = './src'
@@ -15,48 +16,48 @@ const preamble = `/*!
  * ${pkg.name} v${pkg.version}
  * Copyright (C) 2012-${new Date().getFullYear()} ${pkg.author}
  * Date: ${new Date().toUTCString()}
- */`;
+ */`
 
-const extensions = ['.ts', '.js'];
+const extensions = ['.ts', '.js']
 
 const jsPlugins = [
-    resolve(),
-    commonjs({
-        include: 'node_modules/**',
-    }),
-    json(),
-    progress(),
-    filesize({
-        showGzippedSize: true,
-    }),
-    typescript(),
-    babel({
-        babelHelpers: 'bundled',
-        exclude: 'node_modules/**',
-        include: [`${inputPath}/**/*`],
-        extensions,
-    }),
-    terser({ format: { 
-        preamble,
-        comments: false
-    } })
-];
+	resolve(),
+	commonjs({
+		include: 'node_modules/**',
+	}),
+	json(),
+	progress(),
+	filesize({
+		showGzippedSize: true,
+	}),
+	typescript(),
+	babel({
+		babelHelpers: 'bundled',
+		exclude: 'node_modules/**',
+		include: [`${inputPath}/**/*`],
+		extensions,
+	}),
+	terser({ format: { 
+		preamble,
+		comments: false
+	} })
+]
 
 function makeConfig(file, format) {
-    return {
-        input: `${inputPath}/index.ts`,
-        output: {
-            file,
-            format,
-            name: packageName,
-            sourcemap: true,
-        },
-        plugins: jsPlugins
-    };
+	return {
+		input: `${inputPath}/index.ts`,
+		output: {
+			file,
+			format,
+			name: packageName,
+			sourcemap: true,
+		},
+		plugins: jsPlugins
+	}
 }
 
 export default [
-    makeConfig(pkg.main, 'umd'),
-    makeConfig(pkg.module, 'esm'),
-    makeConfig(pkg.browser, 'iife'),
-];
+	makeConfig(pkg.main, 'umd'),
+	makeConfig(pkg.module, 'esm'),
+	makeConfig(pkg.browser, 'iife'),
+]
